ecd70de404b351865825e6057d2ad0f7ba0801ed
root <root@TL>
2024-05-08 16:59:39 +0200

    fifth commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,6 +65,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :description, :start_date, :duration, :price, :user_id)
+      params.require(:event).permit(:title, :description, :start_date, :duration, :price, :location, :user_id)
     end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,90 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:sign_out]
+
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show()
+    unless user_signed_in?
+        id = params[:id]
+        @user = User.find(id)
+    end
+    unless @user
+        redirect_to new_user_session_path
+        flash[:error] = "Tu dois être connecter pour accéder aux détails des événements"
+    end
+end
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # DELETE /users/sign_out
+  def sign_out
+    # Ajoutez ici le code pour déconnecter l'utilisateur
+    # Par exemple, vous pouvez utiliser la méthode fournie par Devise pour déconnecter l'utilisateur
+    sign_out(current_user)
+    redirect_to root_path, notice: 'You have been signed out.'
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id]) unless params[:id] == 'sign_out'
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
